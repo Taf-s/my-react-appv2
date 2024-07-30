81ae3c4b03a4f454776d3d9cdd1faaa2c16ce54c
taf-s <tafara.sithole@gmail.com>
2024-07-30 19:12:12 +0200

    added background

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="bg-white">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Testing Testing
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
