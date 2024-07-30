78608f05bc62e3605ca7f82f3cddb203118cb1eb
taf-s <tafara.sithole@gmail.com>
2024-07-30 20:38:34 +0200

    added first home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center top-44 w-40 h-32 items-center">
+      <h1 className="text-black text-6xl font-bold text-center">
+        Tafara Sithole
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
